ffbc502f7cee9d27fe51c39e47742c3c7a0e17b8
victor mouza <victormouza@gmail.com>
2021-02-05 08:46:25 -0300

    projeto completo

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function linkedineGit() {
+    alert("Vou manda-lo para os melhores websites da internet")
+    window.location.href = "https://www.linkedin.com/in/victor-mouza-a8ab90174/"
+}
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+
+function minhaLista() {
+    alert('VOCE AINDA NÃO TEM UMA LISTA')
+}
